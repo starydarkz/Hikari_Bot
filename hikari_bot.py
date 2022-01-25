1f684e70b01885a203ea98bbcb070211ed37c6e3
StaryDark <staryzly@gmail.com>
2022-01-25 15:27:27 -0600

    Limpieza de codigo, Nuevo comando /rules
    Limpieza de codigo y mejor organizamiento de archivos.
    
    Se añadio un nuevo comando: /rules

--- hikari_bot.py
+++ hikari_bot.py
@@ -17,12 +17,28 @@ def start(update, context):
 
 def help(update, context):
     """Acerca de Hikari Bot"""
+    file = open ('Resources/help.txt','r')
+    help = file.read()
+    file.close()
     context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=functions.help())
+    context.bot.send_message(chat_id=update.effective_chat.id, text=help)
+
+def rules(update, context):
+    """Reglas de la comunidad AprenderPython"""
+    file = open ('Resources/rules.txt','r')
+    rules = file.read()
+    file.close()
+    context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=rules)
+
 
 #Listeners 
 start_handler = CommandHandler("start", start)
 help_handler = CommandHandler("help", help)
+rules_handler = CommandHandler("rules", rules)
+
 updater.dispatcher.add_handler(start_handler)
 updater.dispatcher.add_handler(help_handler)
+updater.dispatcher.add_handler(rules_handler)
+
 updater.start_polling()
