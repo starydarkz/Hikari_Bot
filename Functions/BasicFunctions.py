f69e2dee62fb9c3fa6774ce8fd1dc2ef3f382f8c
StaryDark <staryzly@gmail.com>
2022-02-02 22:59:52 -0400

    Nueva funcion, quees

--- Functions/BasicFunctions.py
+++ Functions/BasicFunctions.py
@@ -1,5 +1,22 @@
-class functions:
-    def config():
-        """Configuracion del bot"""
-        token = input("Escribe el token: ")
-        return token
+def config():
+    """Configuracion del bot"""
+    token = input("Escribe el token: ")
+    return token
+def definiciones(palabra):
+    ''' Busquedas de definiciones en wikipedia '''
+      
+    from Resources.definiciones import definiciones
+    if palabra in definiciones.keys():
+        return definiciones[palabra]
+    else:
+        import wikipedia
+        wikipedia.set_lang("es")
+     
+        try: 
+            resumen = wikipedia.summary(palabra, sentences = 1)
+            url = (wikipedia.page(palabra).url)
+            resultado = (resumen + "\n\nMas info: " + url)
+            return resultado
+        except: 
+            error =  ("No se ha encontrado esta definicion...")
+            return error
--- hikari_bot.py
+++ hikari_bot.py
@@ -2,10 +2,12 @@
 
 from telegram.ext import Updater, CommandHandler
 from telegram import update, ChatAction
-from Functions.BasicFunctions import functions
+from Functions.BasicFunctions import config, definiciones
+
+
 import random
 
-token = functions.config()
+token = config()
 updater = Updater(token=token, use_context=True)
 
 #Commands
@@ -31,14 +33,22 @@ def rules(update, context):
     context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
     context.bot.send_message(chat_id=update.effective_chat.id, text=rules)
 
+def quees(update, context):
+    context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
+    user_say = " ".join(context.args)
+    answer = definiciones(user_say)
+    update.message.reply_text(answer)   
+
 
 #Listeners 
 start_handler = CommandHandler("start", start)
 help_handler = CommandHandler("help", help)
 rules_handler = CommandHandler("rules", rules)
+quees_handler = CommandHandler("quees", quees)
 
 updater.dispatcher.add_handler(start_handler)
 updater.dispatcher.add_handler(help_handler)
 updater.dispatcher.add_handler(rules_handler)
+updater.dispatcher.add_handler(quees_handler)
 
 updater.start_polling()
